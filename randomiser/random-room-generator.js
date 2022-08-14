5d4bbf3e24fe5f579313745aa7177cf138cbfa31
alevans99 <alevans99@gmail.com>
2022-08-14 17:03:40 +0100

    Adds random room name generator

---
+++ randomiser/random-room-generator.js
@@ -0,0 +1,23 @@
+const adjectives = require('./adjectives')
+const nouns = require('./nouns')
+
+/**
+ * Creates a random kebab-case combination of adjectives and a noun.
+ * @returns A randomly created string.
+ */
+const randomRoomGenerator = () => {
+    const adjectiveLength = adjectives.length
+    const nounLength = nouns.length
+    const randomArray = []
+    const numberOfAdjectives = Math.floor(Math.random() * 3) + 2
+
+    for (let i = 0; i < numberOfAdjectives; i++){
+        randomArray.push(adjectives[Math.floor(Math.random() * adjectiveLength)])
+    }
+
+    randomArray.push(nouns[Math.floor(Math.random() * nounLength)])
+    return randomArray.join('-').toLowerCase()
+ 
+}
+
+module.exports = randomRoomGenerator
